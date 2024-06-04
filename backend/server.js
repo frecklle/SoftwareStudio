67f2ca7c7d3cc20fc0915b3ae528ec4fe1aff642
DESKTOP-R0HE8GU\Olek <oracicki@gmail.com>
2024-06-04 20:43:39 +0200

    Update z wierszem

--- backend/server.js
+++ backend/server.js
@@ -1,82 +1,112 @@
- if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
- }
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
 //const db = require("./database.js");
-var cors = require('cors');
-const express = require("express")
+var cors = require("cors");
+const express = require("express");
 const app = express();
 app.use(cors());
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require("express-ejs-layouts");
 
-app.set("view engine", "ejs")
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+app.set("view engine", "ejs");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL)
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log("connected mongoose"))
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("connected mongoose"));
 
 const port = process.env.PORT || 8000;
-const indexRouter = require('./routes/index')
-const userRouter = require('./')
-const database = require( "./database");
+const indexRouter = require("./routes/index");
+const userRouter = require("./");
+const database = require("./database");
 const emailService = require("./emailService");
-const DatabaseController = require('./database');
-const changePassword = require('./changePassword')
-const bcrypt = require('bcrypt')
+const DatabaseController = require("./database");
+const changePassword = require("./changePassword");
+const bcrypt = require("bcrypt");
 
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get('/db', async (req,res)=>{
+app.get("/db", async (req, res) => {
   var items = await database.listItems("users", {});
-  console.log(items)
-  
+  console.log(items);
+
   res.json(items);
-})
+});
 
 //registering
-app.post('/register', async (req,res)=>{
-  var obj = req.body
-  console.log(obj)
-  
+app.post("/register", async (req, res) => {
+  var obj = req.body;
+  console.log(obj);
+
   const existingUser = await database.listItems("users", { email: obj.email });
   if (existingUser.length > 0) {
     // 406 http code Not Acceptable
-    res.status(406).json({ error: 'Email already in use' });
+    res.status(406).json({ error: "Email already in use" });
     return;
   }
 
   var addedItem = await database.addItem("users", obj);
   res.json(addedItem);
-})
+});
 
 //loggingin
-app.post('/login', async (req,res)=>{
-  var obj = req.body
-  console.log(obj)
-  if (obj.email == null || obj.password == null || obj.email == "" || obj.password == ""){
+app.post("/login", async (req, res) => {
+  var obj = req.body;
+  console.log(obj);
+  if (
+    obj.email == null ||
+    obj.password == null ||
+    obj.email == "" ||
+    obj.password == ""
+  ) {
     res.json("fail");
   }
 
-  var foundItems = await database.listItems("users", {email: obj.email, password: obj.password});
+  var foundItems = await database.listItems("users", {
+    email: obj.email,
+    password: obj.password,
+  });
   console.log(foundItems);
   if (foundItems.length > 0) {
-    var o ={status:"success"}
+    let token = btoa(foundItems[0].email);
+
+    var o = { status: "success", token: token };
     res.status(200).json(o);
   } else {
-
-    var o ={status:"fail"}
+    var o = { status: "fail" };
     res.json(o);
   }
-})
+});
+
+// Tylko mam do czynienia z moim niezmienniczonym ciałem,
+// Dłonie, które tak dobrze potrafią dotknąć
+// Cienkich piersi, a także uroczą twarz,
+// Z którą tak dobrze się przyjaźniłem.
+// Czuję, jak mój penis zaczyna się rozprężać,
+// Więząc się od zamiłowania i oczekiwania.
+// Czuję, jak mój brzuch się napięa,
+// Roztaczając się, jak kobieta,
+// Odpowiadając na każde gest, każde dotknięcie.
+// Oczy są tak pełne żądza,
+// Dłonie tak delikatne, jak tkawica.
+// Włosy tak długie, jak halo,
+// Oczy tak jasne, jak gwiazdy.
+// Serce tak nieustraszone, jak ptaszek,
+// Ciało tak delikatne, jak lód.
+// Czuję, jak mój penis zaczyna się rozprężać,
+// Tyleż samo, jak mój zamiłowany wymiar.
+// Czuję, jak mój brzuch się napięa,
+// Tyleż samo, jak mój wymiar.
+// Tyleż samo, jak mój wymiar.
 
 //forgotpassword
-app.post('/forgot-password', async (req, res) => {
+app.post("/forgot-password", async (req, res) => {
   try {
     const { email } = req.body;
 
@@ -84,119 +114,129 @@ app.post('/forgot-password', async (req, res) => {
 
     if (existingUser.length > 0) {
       await emailService.sendResetPasswordEmail(email);
-      var o ={status:"success"}
+      var o = { status: "success" };
       res.status(200).json(o);
     } else {
-      var o ={status:"fail"}
+      var o = { status: "fail" };
       res.json(o);
     }
   } catch (error) {
     console.error("Error checking email:", error);
     res.status(500).json({ error: "Internal Server Error" });
   }
-  
 });
 
 //change-pass
-app.post('/change-password', async (req, res) => {
-    const {email, newPassword, repeatPassword } = req.body;
-    if (newPassword !== repeatPassword) {
-      return res.status(400).json({ error: 'Passwords do not match' });
+app.post("/change-password", async (req, res) => {
+  const { email, newPassword, repeatPassword } = req.body;
+  if (newPassword !== repeatPassword) {
+    return res.status(400).json({ error: "Passwords do not match" });
+  }
+  try {
+    // Find user by email
+    const user = await DatabaseController.findOne({ email });
+
+    // Check if user exists
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
     }
-    try {
-      // Find user by email
-      const user = await DatabaseController.findOne({ email });
 
-      // Check if user exists
-      if (!user) {
-          return res.status(404).json({ error: 'User not found' });
-      }
+    await changePassword.changePass(email, newPassword);
 
-      await changePassword.changePass(email, newPassword)
-
-      return res.status(200).json({ message: 'Password updated successfully' });
+    return res.status(200).json({ message: "Password updated successfully" });
   } catch (error) {
-      console.error('Error updating password:', error);
-      return res.status(500).json({ error: 'Internal server error' });
+    console.error("Error updating password:", error);
+    return res.status(500).json({ error: "Internal server error" });
   }
 });
 
-
 //post Funkcja dodaje nowy wpis do bazy danych.
-app.post('/post', async (input, output) => {
-  
-    var obj = input.body // json przesłany w requescie w frontendzie
-    // console.log(obj) 
-    
-    // sprawdzasz czy pola sa puste
-    if (obj.title == undefined || obj.content == undefined || obj.draft == undefined || obj.archived == undefined){
-      output.json("fail");
-      return;
-    }
+app.post("/post", async (input, output) => {
+  var obj = input.body; // json przesłany w requescie w frontendzie
+  // console.log(obj)
 
-    // ustawiasz data utworzenia
-    obj['createdDate'] = new Date();
-    // dodajesz wpis do bazy danych zapisujac go do tablicy posts
-    var addedItem = await database.addItem("posts", obj);
-    // zwracasz nowy obiekt z bazy danych w formatie json do frontendu
-    output.json(addedItem);
+  // sprawdzasz czy pola sa puste
+  if (
+    obj.title == undefined ||
+    obj.content == undefined ||
+    obj.draft == undefined ||
+    obj.archived == undefined
+  ) {
+    output.json("fail");
+    return;
+  }
+
+  //ustaw userId na poscie z tokena
+  console.log(input.headers);
+  obj["userEmail"] = atob(input.headers["Authorization"]);
+
+  // ustawiasz data utworzenia
+  obj["createdDate"] = new Date();
+  // dodajesz wpis do bazy danych zapisujac go do tablicy posts
+  var addedItem = await database.addItem("posts", obj);
+  // zwracasz nowy obiekt z bazy danych w formatie json do frontendu
+  output.json(addedItem);
 });
 
 //list funkcja zwraca wszystkie wpisy z bazy danych w formacie json posortowane wg pola createdDate czyli data utworzenia
- app.get('/posts', async (req,res)=>{
-   var items = await database.wylistujObjekty("posts", {});
-   // console.log(items)
-    //  sort items by property createdDate
-   items = items.sort((a,b) => {
+app.get("/posts", async (req, res) => {
+  var items = await database.wylistujObjekty("posts", {});
+  // console.log(items)
+  //  sort items by property createdDate
+  items = items.sort((a, b) => {
     if (a.createdDate > b.createdDate) {
-      return -1; 
+      return -1;
     } else if (a.createdDate < b.createdDate) {
-      return 1; 
+      return 1;
     } else {
-      return 0; 
+      return 0;
     }
-   });
-   console.log(items);
-
-
-   res.json(items);
+  });
+  console.log(items);
+  res.json(items);
 });
-
 
 // app.put('/post', async (input, output) => {
 //   var obj = input.body // json przesłany w requescie w frontendzie
-//   console.log(obj) 
+//   console.log(obj)
 //   var updatedItem = await database.updateItem(DbConstants.PostsTable, obj);
 //   output.json(updatedItem);
 // });
 
 // dodaj komentarz do posta jako attrybut "comments" w obiekcie post
-app.post('/comment', async (input, output) => {
-  var obj = input.body // json przesłany w requescie w frontendzie
-  console.log(obj);
-  if (obj.postId == undefined || obj.content == undefined || obj.userId == undefined){
+app.post("/comment", async (input, output) => {
+  var obj = input.body; // json przesłany w requescie w frontendzie
+  // console.log(obj);
+  if (
+    obj.postId == undefined ||
+    obj.content == undefined ||
+    obj.userId == undefined
+  ) {
     output.json("fail");
     return;
   }
   // dodaj komentarz do posta jako attrybut "comments" w obiekcie post
   //pobierz post po attrybucie _id
-  var post = await database.findOne("posts", {_id: new mongoose.Types.ObjectId(obj.postId)});
+  var post = await database.findOne("posts", {
+    _id: new mongoose.Types.ObjectId(obj.postId),
+  });
 
-  if (post["comments"] == undefined)
-    post["comments"] = [];    
-  
+  if (post["comments"] == undefined) post["comments"] = [];
+
   delete obj["postId"];
-  obj['createdDate'] = new Date();
+  obj["createdDate"] = new Date();
   post["comments"] = [obj, ...post["comments"]];
 
-  console.log(post);
+  // console.log(post);
   //zaktualizuj post w bazie danych
-  var updatedItem = await database.updateItem("posts",{_id: new mongoose.Types.ObjectId(obj.postId)}, { $set: { comments: post["comments"] } });
+  var updatedItem = await database.updateItem(
+    "posts",
+    { _id: new mongoose.Types.ObjectId(obj.postId) },
+    { $set: { comments: post["comments"] } }
+  );
   output.json(updatedItem);
 });
 
-
-
-app.listen(port,() =>{
-  console.log(`Server started at http://localhost:${port}`)
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
 });
--- frontend/src/app/home/page.js
+++ frontend/src/app/home/page.js
@@ -1,209 +1,231 @@
 "use client";
-import React, { useState, useEffect } from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
-import AppLogo from '@/components/appLogo';
-import Link from 'next/link';
+import React, { useState, useEffect } from "react";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from "recharts";
+import AppLogo from "@/components/appLogo";
+import Link from "next/link";
 
 function HomePage() {
+  const [posts, setPosts] = useState([]);
+  const [openedCommentId, setOpenedCommentId] = useState("");
+  const [commentContent, setCommentContent] = useState("");
 
-const [posts, setPosts] = useState([])
-const [openedCommentId, setOpenedCommentId] = useState('');
-const [commentContent, setCommentContent] = useState('');
+  const user = {
+    name: "John Doe",
+  };
 
-    const user = {
-        name: "John Doe"
-    };
+  // jest wywolany tylko raz w momencie wyswietlenia strony
+  useEffect(() => {
+    // Pobranie danych z serwera async i zapisanie ich w state
+    async function fetchData() {
+      let postyPobraneZBazyDanych = await fetch("http://localhost:8000/posts", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }).then((response) => response.json());
 
-    // jest wywolany tylko raz w momencie wyswietlenia strony
-    useEffect(() => {
+      setPosts(postyPobraneZBazyDanych);
+    }
 
-        // Pobranie danych z serwera async i zapisanie ich w state
-        async function fetchData() {
-        
-            let postyPobraneZBazyDanych = await fetch("http://localhost:8000/posts",
-                {
-                    method: "GET",
-                    headers: {
-                        "Content-Type": "application/json"
-                    }
-                }
-            ).then(response => response.json());
+    fetchData();
+  }, []);
 
-            setPosts(postyPobraneZBazyDanych);
+  // funkcja do dodawania komentarza
+  const handleCommentSubmission = async (id) => {
+    try {
+      // Make a POST request to your backend API endpoint
+      const response = await fetch("http://localhost:8000/comment", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          userId: "663bd125f773f144a9992ff5",
+          postId: id,
+          content: commentContent,
+        }),
+      });
+    } catch (error) {
+      alert("Error submitting comment: " + error);
+    }
+  };
 
-        }
-  
-        fetchData();
-    }, []);
-    
+  // funkcja do otwierania komentarza
+  const handleCommentOpen = (id) => {
+    setOpenedCommentId(id);
+  };
 
+  const data = [
+    { name: "Jan", spending: 400 },
+    { name: "Feb", spending: 300 },
+    { name: "Mar", spending: 500 },
+    { name: "Apr", spending: 700 },
+    { name: "May", spending: 600 },
+  ];
 
-    // funkcja do dodawania komentarza
-    const handleCommentSubmission = async (id) => {
-
-        try {
-         // Make a POST request to your backend API endpoint
-        const response = await fetch("http://localhost:8000/comment", {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify({
-                    userId: "663bd125f773f144a9992ff5",
-                    postId: id,
-                    content: commentContent
-                })
-            });
-        } catch (error) {
-            alert("Error submitting comment: " + error);
-        }
-    };
-
-    // funkcja do otwierania komentarza
-    const handleCommentOpen = (id) => {
-        setOpenedCommentId(id);
-    };
-
-    const data = [
-        { name: 'Jan', spending: 400 },
-        { name: 'Feb', spending: 300 },
-        { name: 'Mar', spending: 500 },
-        { name: 'Apr', spending: 700 },
-        { name: 'May', spending: 600 },
-    ];
-
-    return (
-        <div className="container">
-            <header className="py-3 header">
-                <div className="container-fluid">
-                    <div className="row align-items-start justify-content-between">
-                        <div className="col-md-4 align-self-center">
-                            <a className="text-dark logo" href="#">Prosper.Net</a>
-                        </div>
-                        <div className="col-md-4 text-center">
-                            <h2 className="text-dark title">Work Earn Learn</h2>
-                        </div>
-                        <div className="col-md-4 d-flex justify-content-end align-items-center">
-                            <div className="footer-text mr-3">
-                                <p className="text-muted mb-0">Welcome, </p>
-                                <Link href="/profile">
-                                    <AppLogo/>
-                                </Link>
-                            </div>
-                            <div>
-                                <a className="btn btn-sm btn-outline-secondary" href="/login">Sign out</a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </header>
-
-
-            <main role="main" className="container main">
-                <div className="row">
-                    <div className="col-md-8">
-                        <div className="main-content">
-                            <div className="mb-5">
-                                <h2 className="font-weight-bold">Posts</h2>
-                            
-
-                            {posts.map(o => {
-                                return (<div className="post">
-                                <div className="post-header">
-                                    <h2>{o.title}</h2>
-                                    <p>{o.dateCreated}</p>
-                                </div>
-                                <div className="post-content">
-                                    <p>{o.content}</p>
-                                </div>
-                                <div className="post-actions">
-                                    <button className="btn btn-sm btn-outline-primary">&#9829;</button>
-                                    <button className="btn btn-sm btn-outline-secondary" onClick={() => handleCommentOpen(o._id)}>Comment</button>
-                                    
-
-                                    {openedCommentId === o._id && <div>
-                                        <input id={o._id} value={commentContent} className='comment-input rounded-lg' onChange={e => setCommentContent(e.target.value)}></input>
-                                    <button className="btn btn-sm btn-secondary rounded-lg" onClick={() => handleCommentSubmission(o._id)}>✅</button>
-                                    </div>}
-                                    
-
-                                    {o.comments  && o.comments.map(c => {
-                                        return (<div className="comment">
-                                            
-                                            <p>{c.content}</p>
-                                        </div>);
-                                    })}
-                                    
-
-                                </div>
-                            </div>);
-                            })}
-
-                        
-
-                        {/* Add more posts with advice */}
-
-                        <div className="mt-5"></div> {/* Space between posts */}
-
-                        
-                        </div>
-
-                        {/* Repeat the post structure for more posts */}
-                    </div>
-
-                    </div>
-                    
-            
-
-                    <div className="col-md-4">
-                        <div className="p-3 mb-3 bg-light rounded">
-                            <h4 className="font-weight-bold">About</h4>
-                            <p className="mb-0">Prosper.Net is a vibrant online community where members can share their insights and experiences on wealth accumulation and financial well-being. It is a platform designed to inspire and empower individuals to achieve their financial goals.</p>
-                            <a href="/bot" class="float-end">
-                                Spending analysis 
-                            </a> 
-                        </div>
-
-                        <div className="p-3 mb-3 bg-light rounded">
-                            <h4 className="font-weight-bold">Create Post</h4>
-                            <button className="btn btn-primary" onClick={() => window.location.href='/post'}>
-                                Create a Post 
-                                
-                            </button>
-                        </div>
-
-
-
-                        
-                    </div>
-                </div>
-                
-
-            </main>
-
-            <footer className="footer">
-                <div className="container">
-                    <div className="row justify-content-end">
-                        <div className="col-md-7 text-right">
-                            <p>2024 Prosper.Net</p>
-                        </div>
-                    </div>
-                </div>
-            </footer>
+  return (
+    <div className="container">
+      <header className="py-3 header">
+        <div className="container-fluid">
+          <div className="row align-items-start justify-content-between">
+            <div className="col-md-4 align-self-center">
+              <a className="text-dark logo" href="#">
+                Prosper.Net
+              </a>
+            </div>
+            <div className="col-md-4 text-center">
+              <h2 className="text-dark title">Work Earn Learn</h2>
+            </div>
+            <div className="col-md-4 d-flex justify-content-end align-items-center">
+              <div className="footer-text mr-3">
+                <p className="text-muted mb-0">Welcome, </p>
+                <Link href="/profile">
+                  <AppLogo />
+                </Link>
+              </div>
+              <div>
+                <a
+                  className="btn btn-sm btn-outline-secondary"
+                  onClick={() => {
+                    localStorage.removeItem("token");
+                  }}
+                  href="/login"
+                >
+                  Sign out
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
-    )
+      </header>
+
+      <main role="main" className="container main">
+        <div className="row">
+          <div className="col-md-8">
+            <div className="main-content">
+              <div className="mb-5">
+                <h2 className="font-weight-bold">Posts</h2>
+                {posts.map((o) => {
+                  return (
+                    <div className="post">
+                      <div className="post-header">
+                        <h2>{o.title}</h2>
+                        <p>{o.dateCreated}</p>
+                      </div>
+                      <div className="post-content">
+                        <p>{o.content}</p>
+                      </div>
+                      <div className="post-actions">
+                        <button className="btn btn-sm btn-outline-primary">
+                          &#9829;
+                        </button>
+                        <button
+                          className="btn btn-sm btn-outline-secondary"
+                          onClick={() => handleCommentOpen(o._id)}
+                        >
+                          Comment
+                        </button>
+
+                        {openedCommentId === o._id && (
+                          <div>
+                            <input
+                              id={o._id}
+                              value={commentContent}
+                              className="comment-input rounded-lg"
+                              onChange={(e) =>
+                                setCommentContent(e.target.value)
+                              }
+                            ></input>
+                            <button
+                              className="btn btn-sm btn-secondary rounded-lg"
+                              onClick={() => handleCommentSubmission(o._id)}
+                            >
+                              ✅
+                            </button>
+                          </div>
+                        )}
+
+                        {o.comments &&
+                          o.comments.map((c) => {
+                            return (
+                              <div className="comment">
+                                <p>{c.content}</p>
+                              </div>
+                            );
+                          })}
+                      </div>
+                    </div>
+                  );
+                })}
+                {/* Add more posts with advice */}
+                <div className="mt-5"></div> {/* Space between posts */}
+              </div>
+
+              {/* Repeat the post structure for more posts */}
+            </div>
+          </div>
+
+          <div className="col-md-4">
+            <div className="p-3 mb-3 bg-light rounded">
+              <h4 className="font-weight-bold">About</h4>
+              <p className="mb-0">
+                Prosper.Net is a vibrant online community where members can
+                share their insights and experiences on wealth accumulation and
+                financial well-being. It is a platform designed to inspire and
+                empower individuals to achieve their financial goals.
+              </p>
+              <a href="/bot" class="float-end">
+                Spending analysis
+              </a>
+            </div>
+
+            <div className="p-3 mb-3 bg-light rounded">
+              <h4 className="font-weight-bold">Create Post</h4>
+              <button
+                className="btn btn-primary"
+                onClick={() => (window.location.href = "/post")}
+              >
+                Create a Post
+              </button>
+            </div>
+          </div>
+        </div>
+      </main>
+
+      <footer className="footer">
+        <div className="container">
+          <div className="row justify-content-end">
+            <div className="col-md-7 text-right">
+              <p>2024 Prosper.Net</p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
-
-                        <div className="p-3">
-                            <h4 className="font-italic">Elsewhere</h4>
-                            <ol className="list-unstyled">
-                                <li><a href="#">GitHub</a></li>
-                                <li><a href="#">Twitter</a></li>
-                                <li><a href="#">Facebook</a></li>
-                            </ol>
-                        </div>
-                
-
+<div className="p-3">
+  <h4 className="font-italic">Elsewhere</h4>
+  <ol className="list-unstyled">
+    <li>
+      <a href="#">GitHub</a>
+    </li>
+    <li>
+      <a href="#">Twitter</a>
+    </li>
+    <li>
+      <a href="#">Facebook</a>
+    </li>
+  </ol>
+</div>;
 
 export default HomePage;
